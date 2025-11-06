66cbb5644a6c74aa126c1b46034f16c29cafb23c
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 14:46:27 +0700

    membuat list pasangan label dan nilai untuk tampilan data pengguna

--- app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
@@ -2,9 +2,17 @@ package com.example.questnavigasiui_236.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import kotlin.io.path.Path
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:() -> unit
-){}
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+}
