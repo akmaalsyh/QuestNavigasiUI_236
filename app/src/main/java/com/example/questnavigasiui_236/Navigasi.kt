7a10891eb527c3d0770bfe568823364e5f4a9b44
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 09:15:34 +0700

    membuat composable DataApp dengan NavHostController untuk mengatur navigasi antar halaman

--- app/src/main/java/com/example/questnavigasiui_236/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_236/Navigasi.kt
@@ -1,6 +1,19 @@
 package com.example.questnavigasiui_236
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@Composable
+fun DataApp (
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){}
