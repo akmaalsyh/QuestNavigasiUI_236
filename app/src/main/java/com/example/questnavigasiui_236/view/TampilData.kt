b9e7f6892d64958c85d822f1c204113c45cd0b4f
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 14:52:11 +0700

    menambahkan Scaffold dan TopAppBar pada halaman TampilData

--- app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
@@ -1,7 +1,12 @@
 package com.example.questnavigasiui_236.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import kotlin.io.path.Path
 
@@ -15,4 +20,11 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+        }){}
 }
