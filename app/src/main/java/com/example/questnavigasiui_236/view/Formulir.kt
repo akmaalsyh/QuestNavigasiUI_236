66d9a353ad42bf00c9f9d38bf44f1f65b7ae9bc8
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 14:16:06 +0700

    menambahkan komponen input Nama Lengkap pada tampilan Formulir

--- app/src/main/java/com/example/questnavigasiui_236/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_236/view/Formulir.kt
@@ -1,10 +1,16 @@
 package com.example.questnavigasiui_236.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -23,5 +29,20 @@ fun FormIsian(
                 colors = TopAppBarDefaults,
                 topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
             ) }
-    ) {}
+    ) { isiRuang ->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+        }
+
+    }
 }
