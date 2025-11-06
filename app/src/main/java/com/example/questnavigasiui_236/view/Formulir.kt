29ac1ec2e0c9257bb71039dce51034f39a0289af
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 14:18:33 +0700

    menambahkan garis pemisah horizontal merah pada tampilan Formulir

--- app/src/main/java/com/example/questnavigasiui_236/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_236/view/Formulir.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -42,6 +43,11 @@ fun FormIsian(
                 label = {Text(text = "Nama Lengkap")},
                 onValueChange = {},
             )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color =
+                Color.Red)
+
         }
 
     }
