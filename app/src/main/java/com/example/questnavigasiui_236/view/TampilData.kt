aae6f7bd5351d276f207d74aee8d4a1f1121d75f
akmaalsyh <taufansyah50@gmail.com>
2025-11-06 14:40:40 +0700

    menambahkan composable TampilData dengan parameter tombol kembali

--- app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_236/view/TampilData.kt
@@ -1,2 +1,10 @@
 package com.example.questnavigasiui_236.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:() -> unit
+){}
